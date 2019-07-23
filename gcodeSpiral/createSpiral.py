4e49b1cefc71dc6b90f7e40458e367e265db97a7
Chris <39500274+cdlindsay17@users.noreply.github.com>
2019-07-23 15:44:47 -0700

    tall spiral

--- gcodeSpiral/createSpiral.py
+++ gcodeSpiral/createSpiral.py
@@ -3,8 +3,8 @@
 Initialize parameters
 """
 radius = 3.5                          #Spiral radius [mm]
-height = 8                        #Spiral height [mm]
-nTurns = 2                          #Number of turns of spiral
+height = 1.8                        #Spiral height [mm]
+nTurns = 6                          #Number of turns of spiral
 nPoints = 20                        #Number of points per turn
 pSpeed = 300                        #Printer speed in [mm/min]
 pitch = height/nTurns/nPoints       #Height of each turn [mm]
